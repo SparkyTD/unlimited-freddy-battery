bcbd5678dd4380987fac3fc9a8a2221fc3d74be0
SparkyTD <45818400+SparkyTD@users.noreply.github.com>
2021-12-25 02:11:29 +0200

    Removed "boot sequence", added fancy colors instead

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
+import os
 import time
 import pymem
 import traceback
 from pymem import *
 from pymem.process import *
 from enum import Enum
-import os
-
-
-class ConsoleColor:
-    HEADER = '\\033[95m'
-    OKBLUE = '\\033[94m'
-    OKCYAN = '\\033[96m'
-    OKGREEN = '\\033[92m'
-    WARNING = '\\033[93m'
-    FAIL = '\\033[91m'
-    ENDC = '\\033[0m'
-    BOLD = '\\033[1m'
-    UNDERLINE = '\\033[4m'
+import colorama
+from colorama import Fore, Style
 
 
 class Error(Enum):
@@ -99,87 +89,42 @@ class BotNotFoundException(Exception):
     pass
 
 
-def print_logo():
-    print(" ___ __  ___ __ ___  __  ___   ___ __  _ _____ ___ ___ _____ __  _ __  _ __ __ ___ __  _ _____ ")
-    print("| __/  \\|_  |  \\ __|/  \\| _ \\ | __|  \\| |_   _| __| _ \\_   _/  \\| |  \\| |  V  | __|  \\| |_   _|")
-    print("| _| /\\ |/ /| -< _|| /\\ | v / | _|| | ' | | | | _|| v / | || /\\ | | | ' | \\_/ | _|| | ' | | |  ")
-    print("|_||_||_|___|__/___|_||_|_|_\\ |___|_|\\__| |_| |___|_|_\\ |_||_||_|_|_|\\__|_| |_|___|_|\\__| |_|  ")
-    print("Bot Console [Infinite Freddy Time] v1.0")
-    print(" by Sparky [u/Sparky2199; github.com/SparkyTD]")
-    print("(Press Ctrl+C to exit)\n")
-
-
-def print_log(text, delay=0.0):
-    print(text)
-    time.sleep(delay)
-
-
-def print_intro():
-    print_logo()
-    print_log("Connecting to corporate network...", 1)
-    print_log("Accessing bot control server...", 0.3)
-    print_log("Indexing animatronic database...", 0.8)
-    print_log(" > SELECT * FROM FAZ_BOTS WHERE type = 'GLAMROCK';", 0.2)
-
-    bot_ids = ["FAZ_BOT_FR_FBEAR", "FAZ_BOT_GL_CHICA", "FAZ_BOT_MG_GATOR", "FAZ_BOT_RX_WOLF", "FAZ_BOT_GL_BONNIE"]
-    for i in range(1, 50):
-        bot_ids.append("FAZ_BOT_ENDO_" + str(i))
-    bot_ids.append("FAZ_BOT_BL_UNKNOWN")
-    bot_ids.append("FAZ_BOT_WA_UNKNOWN")
-
-    ip = 1
-    for bot in bot_ids:
-        if bot == "FAZ_BOT_GL_BONNIE":
-            print_log(f"Found '{bot}' at [TIMEOUT]", 1)
-            try:
-                raise BotNotFoundException()
-            except BotNotFoundException as e:
-                traceback.print_exc()
-            time.sleep(1)
-        else:
-            print_log(f"Found '{bot}' at 10.0.1.{ip}")
-        ip += 1
-
-    try:
-        raise FirmwareCorruptException()
-    except FirmwareCorruptException as e:
-        traceback.print_exc()
-
-    print_log("Connecting to FAZ_BOT_FR_FBEAR...", 1)
-    print_log("Connected!", 0.7)
-
-    os.system("cls")
-    print_logo()
+def print_header():
+    print(Fore.GREEN + "Unlimited Freddy Battery v1.0 by Sparky [u/Sparky2199; github.com/SparkyTD]")
+    print(Fore.LIGHTBLACK_EX + "(Press Ctrl+C to exit)\n")
 
 
 if __name__ == '__main__':
-
-    freddy = FreddyProcess()
-    while True:
-        freddy.init()
-        countdown = 5
-        if freddy.is_loaded():
-            print_intro()
-        while freddy.is_loaded():
-            sys.stdout.write(f"Recharging battery in {countdown}...\r")
-            sys.stdout.flush()
-            if countdown == 0:
-                freddy.set_battery_counter(100)
-                sys.stdout.write("Recharged!                                   \r")
-                sys.stdout.flush()
-                countdown = 6
-            countdown -= 1
-            # freddy.set_battery_counter(100)
-            time.sleep(1)
-        if not freddy.is_loaded():
-            error = freddy.get_error()
-            os.system("cls")
-            print_logo()
-            print(f"Connection lost! ({error})")
-            if error == Error.GAME_NOT_FOUND:
-                print("The game does not appear to be running.")
-            elif error == Error.ADDRESS_NOT_FOUND:
-                print("Unable to find Freddy's battery. If you're in the menu, load a save first.")
-                print("If that still doesn't work, check if there is an updated Bot Console at https://github.com/SparkyTD/infinite-freddy-time")
+    try:
+        freddy = FreddyProcess()
+        while True:
             freddy.init()
-            time.sleep(10)
+            countdown = 5
+            os.system("cls")
+            print_header()
+            while freddy.is_loaded():
+                sys.stdout.write(Fore.BLUE + f"Recharging battery in {countdown}...\r")
+                sys.stdout.flush()
+                if countdown == 0:
+                    freddy.set_battery_counter(100)
+                    sys.stdout.write(Fore.GREEN + "Recharged!                                   \r")
+                    sys.stdout.flush()
+                    countdown = 6
+                countdown -= 1
+                # freddy.set_battery_counter(100)
+                time.sleep(1)
+            if not freddy.is_loaded():
+                error = freddy.get_error()
+                os.system("cls")
+                print_header()
+                # print(Fore.RED + f"Connection lost! ({error})")
+                if error == Error.GAME_NOT_FOUND:
+                    print(Fore.RED + "The game does not appear to be running.")
+                elif error == Error.ADDRESS_NOT_FOUND:
+                    print(Fore.RED + "Unable to find Freddy's battery. If you're in the menu, load a save slot first.")
+                    print(Fore.RED + "If that still doesn't work, check if there is an updated Bot Console at https://github.com/SparkyTD/infinite-freddy-time")
+                freddy.init()
+                time.sleep(10)
+    except KeyboardInterrupt:
+        print(Fore.WHITE + "Exiting...")
+        sys.exit(0)
